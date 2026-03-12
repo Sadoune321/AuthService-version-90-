f29e9c1260e092b1101099afb4e4c855c11e44f3
Sadoune321 <Sadounefadjre321@gmail.com>
2026-03-13 00:56:32 +0100

    fix some problems related by the lexical problems DOCT AND PAIT

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { ConfigService } from '@nestjs/config';
+import { Role } from '../common/role.enum';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-
   constructor(private readonly configService: ConfigService) {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID')!,
@@ -13,8 +13,25 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
       scope: ['email', 'profile'],
       passReqToCallback: true,
+      
     });
   }
+
+  
+  authenticate(req: any, options?: any) {
+  let role = req.query?.role ?? Role.PATIENT;
+  
+  if (typeof role === 'string') {
+    role = role.replace(/^"|"$/g, '').trim();
+  }
+  
+  if (!Object.values(Role).includes(role)) {
+    role = Role.PATIENT;
+  }
+
+  const state = Buffer.from(JSON.stringify({ role })).toString('base64');
+  super.authenticate(req, { ...options, state });
+}
 
   async validate(
     req: any,
@@ -25,12 +42,29 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   ) {
     const { id, emails, name } = profile;
 
+    let role = Role.DOCTOR;
+      console.log('=== GOOGLE CALLBACK ===');
+  console.log('req.query:', req.query);
+  console.log('req.query.state:', req.query.state);
+
+    try {
+      const rawState = req.query.state;
+      if (rawState) {
+        const decoded = JSON.parse(Buffer.from(rawState, 'base64').toString());
+        if (decoded.role && Object.values(Role).includes(decoded.role)) {
+          role = decoded.role;
+        }
+      }
+    } catch {
+      role = Role.PATIENT;
+    }
+
     const user = {
       googleId: id,
       email: emails[0].value,
       firstName: name?.givenName || '',
       lastName: name?.familyName || '',
-      role: req.query.role || 'PATIENT',
+      role,
     };
 
     done(null, user);
