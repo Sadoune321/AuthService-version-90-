47f45440ca86573b734a90b8f69f925abb19fa99
Sadoune321 <Sadounefadjre321@gmail.com>
2026-03-19 21:45:48 +0100

    Fix Docker & Redis apres final25

--- src/config/redis.client.ts
+++ src/config/redis.client.ts
@@ -1,23 +1,7 @@
-// src/config/redis.client.ts
 import Redis from 'ioredis';
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-
-  tls: {}, // ✅ مهم لـ Upstash
-
-  maxRetriesPerRequest: null, // ✅ يمنع crash
-  retryStrategy: (times) => Math.min(times * 50, 2000), // ✅ retry ذكي
-});
-
-redisClient.on('connect', () => {
-  console.log('✅ Redis connected');
-});
-
-redisClient.on('error', (err) => {
-  console.error('❌ Redis error:', err.message);
+const redisClient = new Redis(process.env.REDIS_URL!, {
+  tls: {},
 });
 
 export default redisClient;
